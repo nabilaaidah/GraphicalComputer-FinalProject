468af45de3599aacbe4fd7fc1ccc832e17334896
Nabila Diani <aidahdiani@gmail.com>
2023-12-11 14:15:03 +0700

    feat update rotation

--- src/script.js
+++ src/script.js
@@ -90,6 +90,7 @@ const bakedMaterial = new THREE.MeshBasicMaterial({
   map: bakedTexture
 });
 
+// Load Model
 gltfLoader.load('/models/rendersimplebake.glb', gltf => {
   gltf.scene.scale.set(0.3, 0.3, 0.3);
   gltf.scene.position.set(0, -1, 0);
